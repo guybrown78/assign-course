b3252fe2635a42d3465618b7eeee824792ec6ee7
Guy Brown <guy@digitalopolis.co.uk>
2020-08-07 08:21:09 +0100

    added questions save stuff

--- src/app/question-builder/question-builder.component.ts
+++ src/app/question-builder/question-builder.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms"
 export class QuestionBuilderComponent implements OnInit {
 
 	questions:any[] = []
-
+	allQuestionsValid:boolean = false;
 
   constructor() {
 		this.addNewQuestion()
@@ -28,10 +28,12 @@ export class QuestionBuilderComponent implements OnInit {
 				isValid:false,
 			}
 		)
+		this.checkValidation();
 	}
 	removeQuestion(index){
 		// remove that index entry from the array
 		this.questions.splice(index, 1);
+		this.checkValidation();
 	}
 
 	updateQuestionDetails(details, index){
@@ -39,5 +41,20 @@ export class QuestionBuilderComponent implements OnInit {
 		this.questions[index].learningObjective = details.learningObjective;
 		this.questions[index].answers = [ ...details.answers ]
 		this.questions[index].isValid = details.isValid;
+		this.checkValidation();
+	}
+
+	checkValidation(){
+		const vq = this.questions.filter(q => q.isValid);
+		this.allQuestionsValid = vq.length === this.questions.length;
+	}
+	saveQuestions(){
+		console.log(this.questions);
+		// TODO
+	}
+
+	cancel(){
+		console.log("cancel")
+		// TODO
 	}
 }
